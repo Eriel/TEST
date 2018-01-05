4beb80707ff47a5a4aa93a3b030bcb2409edcfcf
Eriel <31312694+Eriel@users.noreply.github.com>
2018-01-05 21:18:44 +0200

    4

--- Test2.js
+++ Test2.js
@@ -6,7 +6,7 @@
 
       var COUNTER_SAS='';
 
-      nativeAd.url='https://meyerweb.com/eric/tools/dencoder/';
+      nativeAd.url= a;
 
       nativeAd.title='%D0%90%D0%BA%D0%BE%20%D0%B8%20%D0%9C%D0%BE%D1%83%D1%80%D0%B8%D0%BD%D1%8C%D0%BE%20%D0%BD%D0%B5%20%D0%B5%20%D1%87%D0%BE%D0%B2%D0%B5%D0%BA%D1%8A%D1%82%2C%20%D0%BA%D0%BE%D0%B9%20%D1%81%D1%82%D0%B0%D0%B2%D0%B0%20%D0%B7%D0%B0%20%D0%9C%D0%B0%D0%BD%D1%87%D0%B5%D1%81%D1%82%D1%8A%D1%80%20%D0%AE%D0%BD%D0%B0%D0%B9%D1%82%D0%B5%D0%B4%3F';
 
