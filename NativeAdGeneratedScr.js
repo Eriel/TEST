6b99aaa47bd7c2ccccff5e83faad4eb5b308b984
Eriel <31312694+Eriel@users.noreply.github.com>
2018-01-06 15:44:32 +0200

    new

--- NativeAdGeneratedScr.js
+++ NativeAdGeneratedScr.js
@@ -1,12 +1,13 @@
 
      window.onload = function() {
-      var x = document.getElementById("NativeAd777");
-      
+
+      var element = document.getElementById("NativeAd777");
+
       var nativeAd = {};
 
-      var COUNTER_SAS= x.getAttribute("ClickCounter");
+      var COUNTER_SAS= element.getAttribute("ClickCounter");
 
-      nativeAd.url= COUNTER_SAS + "https://www.abv.bg/";
+      nativeAd.url= COUNTER_SAS + 'htps://play.7777.bg/news/?aff_id=14&camp_id=47&ev_id=170';
 
       nativeAd.title='%D0%90%D0%BA%D0%BE%20%D0%B8%20%D0%9C%D0%BE%D1%83%D1%80%D0%B8%D0%BD%D1%8C%D0%BE%20%D0%BD%D0%B5%20%D0%B5%20%D1%87%D0%BE%D0%B2%D0%B5%D0%BA%D1%8A%D1%82%2C%20%D0%BA%D0%BE%D0%B9%20%D1%81%D1%82%D0%B0%D0%B2%D0%B0%20%D0%B7%D0%B0%20%D0%9C%D0%B0%D0%BD%D1%87%D0%B5%D1%81%D1%82%D1%8A%D1%80%20%D0%AE%D0%BD%D0%B0%D0%B9%D1%82%D0%B5%D0%B4%3F';
 
@@ -16,8 +17,10 @@
 
       nativeAd.thumbWide='https://ip1.7777.bg/media/play_news/images/2018/1/5/44f36669d511ccdfbab781be17401486951349707/big.jpg';
 
-      nativeAd.hostSite='TESTNATIVE';
+      nativeAd.hostSite='play.7777.bg';
 
       window.top.showNative(nativeAd);
 
     };
+
+    
